b2cd3b7e921bb4d198ddd25523859f9813ea1642
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2025-08-14 12:52:49 +0200

    edit remap for copying current filename

--- macos/.config/nvim/lua/heorhi/remap.lua
+++ macos/.config/nvim/lua/heorhi/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pf", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>fn", "let @+ = expand('%')<cr>")
+
+function insertFullPath()
+	local filepath = vim.fn.expand("%")
+	vim.fn.setreg("+", filepath) -- write to clipboard
+end
+vim.keymap.set("n", "<leader>pc", insertFullPath, { noremap = true, silent = true })
