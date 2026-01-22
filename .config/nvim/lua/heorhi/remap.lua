80c03f4c8867728da82c0b7549ca626ab0e06340
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2026-01-22 21:59:42 +0100

    add starship.toml, add <leader>pc remap to copy filename

--- .config/nvim/lua/heorhi/remap.lua
+++ .config/nvim/lua/heorhi/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pf", vim.cmd.Ex)
+
+function insertFullPath()
+	local filepath = vim.fn.expand("%")
+	vim.fn.setreg("+", filepath) -- write to clipboard
+end
+vim.keymap.set("n", "<leader>pc", insertFullPath, { noremap = true, silent = true })
