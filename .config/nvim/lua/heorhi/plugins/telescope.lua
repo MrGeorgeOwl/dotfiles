e9a355ee5479d446278fe27d1ebe103c694eaa91
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2026-04-27 15:20:09 +0200

    fix: telescope can't find files in the folders with dot at the start (e.g. .config)

--- .config/nvim/lua/heorhi/plugins/telescope.lua
+++ .config/nvim/lua/heorhi/plugins/telescope.lua
@@ -2,8 +2,36 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {
+                    '%.venv/',
+                    '%.git/',
+                },
+            },
+        })
+
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true,
+                no_ignore_parent = true,
+            })
+        end, {})
+        vim.keymap.set('n', '<leader>fg', function()
+            builtin.live_grep({
+                additional_args = function()
+                    return {
+                        '--hidden',
+                        '--no-ignore',
+                        '--glob',
+                        '!**/.venv/**',
+                        '--glob',
+                        '!**/.git/**',
+                    }
+                end,
+            })
+        end, {})
     end
 }
