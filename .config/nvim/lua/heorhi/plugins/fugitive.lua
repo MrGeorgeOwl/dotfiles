282c2c5343e6d87b7ba8446382264b560eff7815
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2026-07-08 13:25:23 +0200

    fix: open project structure from fugitive status

--- .config/nvim/lua/heorhi/plugins/fugitive.lua
+++ .config/nvim/lua/heorhi/plugins/fugitive.lua
@@ -1,11 +1,13 @@
 return {
-    'tpope/vim-fugitive',
-    config = function()
-        local function get_git_status()
-            vim.cmd("only")
-            vim.cmd("Gedit :")
-        end
+	"tpope/vim-fugitive",
+	config = function()
+		local function get_git_status()
+			vim.cmd("Gedit :")
+			if #vim.api.nvim_list_wins() > 1 then
+				vim.cmd("only")
+			end
+		end
 
-        vim.keymap.set("n", "<leader>gs", get_git_status)
-    end
+		vim.keymap.set("n", "<leader>gs", get_git_status)
+	end,
 }
--- .config/nvim/lua/heorhi/remap.lua
+++ .config/nvim/lua/heorhi/remap.lua
@@ -1,5 +1,25 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pf", vim.cmd.Ex)
+
+local function project_structure_root()
+	local buffer_name = vim.api.nvim_buf_get_name(0)
+	local start = vim.fn.getcwd()
+
+	if buffer_name ~= "" and not buffer_name:match("^%w+://") then
+		if vim.fn.isdirectory(buffer_name) == 1 then
+			start = buffer_name
+		elseif vim.fn.filereadable(buffer_name) == 1 then
+			start = vim.fs.dirname(buffer_name)
+		end
+	end
+
+	return vim.fs.root(start, { ".git" }) or start
+end
+
+local function open_project_structure()
+	vim.cmd("Explore " .. vim.fn.fnameescape(project_structure_root()))
+end
+
+vim.keymap.set("n", "<leader>pf", open_project_structure, { desc = "Open project structure" })
 
 function insertFullPath()
 	local filepath = vim.fn.expand("%")
