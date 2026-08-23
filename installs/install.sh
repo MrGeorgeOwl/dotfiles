07f89697113d18bc0b09266c06e18e36891588f5
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2026-08-23 15:26:16 +0200

    feat: remove installs

--- installs/install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update
-sudo apt-get install git xclip btop tree fzf tar unzip zsh curl nvm -y
-snap install spotify
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install bat ripgrep
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-curl -LsSf https://astral.sh/uv/install.sh | sh
